af6282bf22e9e259a389ca44c14cde9b7c4c4be4
Anna Semjen <anna.semjen@datastax.com>
2025-04-15 15:14:13 +0100

    latest version not hardcoding version numbers

--- helm_installation.tf
+++ helm_installation.tf
@@ -42,11 +42,12 @@ resource "helm_release" "mission_control_datastax" {
   name        = "mission-control"
   namespace   = "mission-control"
   repository  = "oci://registry.replicated.com/mission-control"
-  version     = "1.6.2"
+  #version     = "1.6.2"
   chart       = "mission-control"
   create_namespace = true
   repository_username = var.user_email
   repository_password = var.license_id
+  #repository = "./mc-mimir-updated"
   values = [
 
     templatefile(var.helm_override_file, { loki_bucket = "${var.loki_bucket}"
@@ -55,5 +56,34 @@ resource "helm_release" "mission_control_datastax" {
     
   ]
   depends_on = [helm_release.cert_manager]
+}
+
+resource "kubernetes_namespace" "kubernetes_dashboard" {
+  metadata {
+    name = "kubernetes-dashboard"
+  }
+}
+
+
+###
+# Install the Metrics Server using the Helm provider
+###
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = kubernetes_namespace.kubernetes_dashboard.metadata[0].name
+  # Recent updates to the Metrics Server do not work with self-signed certificates by default.
+  # Since Docker For Desktop uses such certificates, you’ll need to allow insecure TLS
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls=true}"
+  }
+  wait = true
+}
+
+# Output metadata of the Metrics Server release
+output "metrics_server_service_metadata" {
+  value = helm_release.metrics_server.metadata
 }
 

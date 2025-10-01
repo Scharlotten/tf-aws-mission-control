60f97cbfae4b7bd5c2c5d147da5a13371f8bc375
Anna Semjen <asemjen@li-104fbf4c-22b9-11b2-a85c-b8e891b359e3.ibm.com>
2025-10-01 10:37:51 +0100

    Updated mission control chart to v 1.15

--- helm_install.tf
+++ helm_install.tf
@@ -36,7 +36,7 @@ resource "helm_release" "mission_control_datastax" {
   name        = "mission-control"
   namespace   = "mission-control"
   repository  = "oci://registry.replicated.com/mission-control"
-  #version     = "1.6.2"
+  version     = "1.15.0"
   chart       = "mission-control"
   create_namespace = true
   repository_username = var.user_email
